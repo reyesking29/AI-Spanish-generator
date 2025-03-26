3f6e2a88cbfa6363a0c505d39a7d9fad947c22a4
reyesking29 <reyesrob29@gmail.com>
2025-03-25 21:02:00 -0700

    API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function generatePoem(event){
-    event.preventDefault();
-
+function displayPoem(response){
     new Typewriter("#poem", {
-        strings: "Estoy enamorada de ti",
+        strings: response.data.answer,
         autoStart: true,
         delay: 1,
         cursor: "",
-    });
+    }); 
+}
+
+function generatePoem(event){
+    event.preventDefault();
+
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "a6fa43b1789f324t7dff7bf43co046f3";
+    let prompt = `User instructions: Generate a Spanish poem about ${instructionsInput.value}`;
+    let context = "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    let apiURL = "https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context={context}&key=${apiKey}";
+
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating">⏳ Generating a Spanish poem about ${instructionsInput.value}</div>`;
+    
+    axios.get(apiURL). then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
